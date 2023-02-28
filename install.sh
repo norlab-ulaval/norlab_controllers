617b752b9807b4ded0d9122cb3b319857ef7ad14
DomBaril <dominic.baril@usherbrooke.ca>
2023-02-28 09:34:29 -0500

    fixed install script to work with default ubuntu22 installation

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-yes | pip uninstall norlabcontrollib
-python setup.py bdist_wheel
-pip install dist/norlabcontrollib-0.1.0-py3-none-any.whl 
+yes | pip3 uninstall norlabcontrollib
+python3 setup.py bdist_wheel
+pip3 install dist/norlabcontrollib-0.1.0-py3-none-any.whl 
 
