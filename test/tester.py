044772ccaf6d7df781ab569032a4dded4c832a95
DomBaril <dominic.baril@usherbrooke.ca>
2023-03-13 14:26:38 -0400

    implimented prediction cost symbolic computation for testing

--- test/tester.py
+++ test/tester.py
@@ -1,25 +1,106 @@
 from norlabcontrollib.controllers.controller_factory import ControllerFactory
 from norlabcontrollib.path.path import Path
+from norlabcontrollib.models.ideal_diff_drive import Ideal_diff_drive
 
-import numpy as np
+import autograd.numpy as np
+from autograd import grad
+import time
+import casadi as cas
+import numdifftools as nd
 
 controller_factory = ControllerFactory()
 controller = controller_factory.load_parameters_from_yaml('test_parameters_smpc.yaml')
-
+#
 test_path_poses = np.load('../traj_a_int.npy')
 test_path = Path(test_path_poses)
 test_path.compute_metrics(controller.path_look_ahead_distance)
-
+#
 controller.update_path(test_path)
-
+#
 init_state = np.zeros(3)
+# init_state[1] = 2
 controller.init_state = init_state
+controller.compute_desired_trajectory(init_state)
+print(controller.target_trajectory)
 # controller.predict_then_compute_cost(controller.previous_input_array)
-controller.compute_command_vector(init_state)
+
+############################ CASADI optimal control test
+x0 = cas.SX(3, 1)
+x0[:, 0] = init_state
+u = cas.SX.sym('u', controller.horizon_length, 2)
+R = cas.SX.eye(3)
+R[0,0] = cas.cos(x0[2, 0])
+R[1,1] = cas.cos(x0[2, 0])
+R[0, 1] = -cas.sin((x0[2, 0]))
+R[1, 0] = cas.sin((x0[2, 0]))
+J = cas.SX(3, 2)
+J[0,:] = controller.wheel_radius / 2
+J[1, :] = 0
+J[2, 0] = -controller.wheel_radius / controller.baseline
+J[2, 1] = controller.wheel_radius / controller.baseline
+
+ideal_dd = Ideal_diff_drive(controller.wheel_radius, controller.baseline, 1/controller.rate)
+idd_state = np.zeros(6)
+
+casadi_x = cas.SX.sym('x', 3)
+casadi_u = cas.SX.sym('u', 2)
+
+R[0, 0] = cas.cos(casadi_x[2])
+R[1, 1] = cas.cos(casadi_x[2])
+R[0, 1] = -cas.sin((casadi_x[2]))
+R[1, 0] = cas.sin((casadi_x[2]))
+x_k = casadi_x + cas.mtimes(R, cas.mtimes(J, casadi_u)) / controller.rate
+
+single_step_pred = cas.Function('single_step_pred', [casadi_x, casadi_u], [x_k])
+x_0 = cas.SX.sym('x_0', 3)
+x_horizon_list = [x_0]
+u_horizon = cas.SX.sym('u_horizon', controller.horizon_length, 2)
+
+x_ref = cas.DM.zeros(3, controller.horizon_length)
+x_ref[:, :] = controller.target_trajectory
+state_cost_matrix = cas.DM.zeros(3,3)
+state_cost_matrix[:, :] = controller.state_cost_matrix
+u_ref = cas.DM.zeros(2, controller.horizon_length)
+u_ref[:, :] = controller.previous_input_array
+u_ref = u_ref.T
+input_cost_matrix = cas.DM.zeros(2,2)
+input_cost_matrix[:, :] = controller.input_cost_matrix
+prediction_cost = cas.SX(0)
+
+for i in range(1, controller.horizon_length):
+    x_horizon_list.append(single_step_pred(x_horizon_list[i-1], u_horizon[i, :]))
+    x_error = x_ref[i]- x_horizon_list[i]
+    state_cost = cas.mtimes(cas.mtimes(x_error.T, state_cost_matrix), x_error)
+    u_error = u_ref[i, :] - u_horizon[i, :]
+    input_cost = cas.mtimes(cas.mtimes(u_error, input_cost_matrix), u_error.T)
+    prediction_cost += state_cost + input_cost
+
+x_horizon = cas.hcat(x_horizon_list)
+horizon_pred = cas.Function('horizon_pred', [x_0, u_horizon], [x_horizon])
+pred_cost = cas.Function('pred_cost', [x_0, u_horizon], [prediction_cost])
+
+casadi_single_step_pred = single_step_pred(init_state, controller.previous_input_array[:, 0])
+idd_single_step_pred = ideal_dd.predict(idd_state, controller.previous_input_array[:, 0])
+
+t0 = time.time()
+for i in range(1, controller.horizon_length):
+    idd_state = ideal_dd.predict(idd_state, controller.previous_input_array[:, i])
+t1 = time.time()
+t_idd = t1 - t0
+#
+# f = Function('f', [u], [x_k])
+t0 = time.time()
+# casadi_idd_pred = horizon_pred(init_state, controller.previous_input_array.T)
+prediction_cost = pred_cost(init_state, controller.previous_input_array.T)
+t1 = time.time()
+t_cas = t1 - t0
+
+
+
+#
+print('time idd : ', t_idd)
+print(idd_state)
+print('time_cas : ', t_cas)
+print(prediction_cost)
+
 print('test')
-
-# controller.compute_command_vector(np.array([0.0, 0.0, 0.0]))
-
-# fig, ax = plt.subplots()
-# ax.plot(test_path.angles)
-# plt.show()
