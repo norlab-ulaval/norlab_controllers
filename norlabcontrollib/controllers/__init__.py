9274d653211955337004acf0066d8db1bed3021e
Damien LaRocque <phicoltan@gmail.com>
2023-11-22 18:19:08 -0500

    Fix controllers imports; provide a public interface to the module

---
+++ norlabcontrollib/controllers/__init__.py
@@ -0,0 +1,9 @@
+from .differential_orthogonal_exponential import DifferentialOrthogonalExponential
+from .differential_rotation_p import DifferentialRotationP
+from .ideal_diff_drive_mpc import IdealDiffDriveMPC
+
+__all__ = (
+    "DifferentialOrthogonalExponential",
+    "DifferentialRotationP",
+    "IdealDiffDriveMPC",
+)
--- norlabcontrollib/controllers/controller_factory.py
+++ norlabcontrollib/controllers/controller_factory.py
@@ -1,19 +1,29 @@
-from norlabcontrollib.controllers.differential_orthogonal_exponential import DifferentialOrthogonalExponential
-from norlabcontrollib.controllers.differential_rotation_p import DifferentialRotationP
-from norlabcontrollib.controllers.ideal_diff_drive_mpc import IdealDiffDriveMPC
 import yaml
+
+from norlabcontrollib.controllers import (
+    DifferentialOrthogonalExponential,
+    DifferentialRotationP,
+    IdealDiffDriveMPC,
+)
 
 
 class ControllerFactory:
     def load_parameters_from_yaml(self, yaml_file_path):
         with open(yaml_file_path) as yaml_file:
             yaml_params = yaml.full_load(yaml_file)
-            if yaml_params['controller_name'] == 'DifferentialOrthogonalExponential':
+            controller_name = yaml_params.get("controller_name", None)
+            if controller_name is None:
+                raise RuntimeError(
+                    f"controller name was not specified in param file {yaml_file_path}"
+                )
+            if controller_name == "DifferentialOrthogonalExponential":
                 controller = DifferentialOrthogonalExponential(yaml_params)
-            elif yaml_params['controller_name'] == 'DifferentialRotationP':
+            elif controller_name == "DifferentialRotationP":
                 controller = DifferentialRotationP(yaml_params)
-            elif yaml_params['controller_name'] == 'IdealDiffDriveMPC':
+            elif controller_name == "IdealDiffDriveMPC":
                 controller = IdealDiffDriveMPC(yaml_params)
             else:
-                raise RuntimeError("Undefined controller, please specify a valid controller name")
+                raise RuntimeError(
+                    "Undefined controller, please specify a valid controller name"
+                )
             return controller
