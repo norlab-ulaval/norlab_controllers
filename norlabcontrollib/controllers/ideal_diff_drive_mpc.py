f77aab8d95d5ff266dc564f120ff3df6c630247f
Jean-Michel Fortin <jmfortin29@gmail.com>
2025-03-07 22:05:27 +0000

    clean-up ideal-diff-drive, to test on robot

--- norlabcontrollib/controllers/ideal_diff_drive_mpc.py
+++ norlabcontrollib/controllers/ideal_diff_drive_mpc.py
@@ -15,11 +15,10 @@ class IdealDiffDriveMPC(Controller):
 
         self.horizon_length = parameter_map['horizon_length']
         self.id_window_size = parameter_map['id_window_size']
-        self.state_cost_translational = parameter_map['state_cost_translational']
-        self.state_cost_rotational = parameter_map['state_cost_rotational']
 
-        self.input_cost_wheel = parameter_map['input_cost_wheel']
-        self.input_cost_matrix_i = np.eye(2) * self.input_cost_wheel
+        self.linear_state_cost = parameter_map['linear_state_cost']
+        self.angular_state_cost = parameter_map['angular_state_cost']
+        self.velocity_delta_cost = parameter_map['velocity_delta_cost']
         
         self.wheel_radius = parameter_map['wheel_radius']
         self.baseline = parameter_map['baseline']
@@ -33,104 +32,86 @@ class IdealDiffDriveMPC(Controller):
         
         self.init_casadi_model()
 
-        self.debug_indicator = [False,False,False]
-
 
     def init_casadi_model(self):
         
-        ### Init value moved to be able to reset casadi init 
-        self.orthogonal_projection_ids_horizon = np.zeros(self.horizon_length).astype('int32')
-        self.orthogonal_projection_dists_horizon = np.zeros(self.horizon_length)
-        self.prediction_input_covariances = np.zeros((2, 2, self.horizon_length))
         self.motion_model = IdealDiffDrive(self.wheel_radius, self.baseline, 1/self.rate)
-        
-        previous_body_vel_input_array = np.zeros((2, self.horizon_length))
-        previous_body_vel_input_array[0, :] = self.maximum_linear_velocity / 2
         self.max_wheel_vel = self.motion_model.compute_wheel_vels(np.array([self.maximum_linear_velocity, 0]))[0]
-        self.previous_input_array = np.zeros((2, self.horizon_length))
-        
-        self.nd_input_array = np.zeros((2, self.horizon_length))
-        self.target_trajectory = np.zeros((3, self.horizon_length))
-        self.optim_trajectory_array = np.zeros((3, self.horizon_length))
-        self.straight_line_input = np.full(2*self.horizon_length, 1.0)
-        self.straight_line_input[self.horizon_length:] = np.full(self.horizon_length, 2.0)
-  
-        self.R = cas.SX.eye(3)
-        self.J = cas.SX(3, 2)
-        self.J = self.motion_model.jacobian_3x2
-
-        self.casadi_x = cas.SX.sym('x', 3)
-        self.casadi_u = cas.SX.sym('u', 2)
-
-        
-        self.R[0, 0] = cas.cos(self.casadi_x[2])
-        self.R[1, 1] = cas.cos(self.casadi_x[2])
-        self.R[0, 1] = -cas.sin((self.casadi_x[2]))
-        self.R[1, 0] = cas.sin((self.casadi_x[2]))
-        self.R_inv = self.R.T
-
-        self.x_k = self.casadi_x + cas.mtimes(self.R, cas.mtimes(self.J, self.casadi_u)) / self.rate
-        self.single_step_pred = cas.Function('single_step_pred', [self.casadi_x, self.casadi_u], [self.x_k])
-
-        self.x_0 = cas.SX.sym('x_0', 3, 1)
-        self.x_horizon_list = [self.x_0]
-        self.u_horizon_flat = cas.SX.sym('u_horizon_flat', 2 * self.horizon_length)
-        self.u_horizon = cas.SX(self.horizon_length, 2)
-        self.u_horizon[:, 0] = self.u_horizon_flat[:self.horizon_length]
-        self.u_horizon[:, 1] = self.u_horizon_flat[self.horizon_length:]
-
-        # self.x_ref = cas.DM.zeros(3, self.horizon_length)
-        self.x_ref_flat = cas.SX.sym('x_ref', 3 * self.horizon_length)
-        self.x_ref = cas.SX.zeros(3, self.horizon_length)
-        self.x_ref[0, :] = self.x_ref_flat[:self.horizon_length]
-        self.x_ref[1, :] = self.x_ref_flat[self.horizon_length:2 * self.horizon_length]
-        self.x_ref[2, :] = self.x_ref_flat[2 * self.horizon_length:3 * self.horizon_length]
-        # self.x_ref[:, :] = self.target_trajectory
-        
-        self.u_ref = cas.DM.zeros(2, self.horizon_length)
-        self.u_ref[:, :] = self.previous_input_array
-        self.u_ref = self.u_ref.T
-
-        # Change self.cas_input_cost_matrix as SX
-        self.cas_input_cost_param = cas.SX.sym('input_cost_param', 1)
-        self.cas_input_cost_matrix = cas.SX.eye(2)
-        self.cas_input_cost_matrix[0,0] = self.cas_input_cost_param
-        self.cas_input_cost_matrix[1,1] = self.cas_input_cost_param
-
-        self.cas_state_cost_translational = cas.SX.sym('state_cost_translationnal', 1)
-        self.cas_state_cost_rotationnal = cas.SX.sym('state_cost_rotationnal', 1)
-        
-        self.cas_state_cost_matrix = cas.SX.eye(3)
-        self.cas_state_cost_matrix[0,0] = self.cas_state_cost_translational
-        self.cas_state_cost_matrix[1,1] = self.cas_state_cost_translational
-        self.cas_state_cost_matrix[2,2] = self.cas_state_cost_rotationnal
-        
-        self.prediction_cost = cas.SX(0)
-
-        for i in range(1, self.horizon_length):
-            self.x_horizon_list.append(self.single_step_pred(self.x_horizon_list[i - 1], self.u_horizon[i - 1, :]))
-            x_error = self.x_ref[:, i] - self.x_horizon_list[i]
-            x_error[2] = cas.atan2(cas.sin(x_error[2]), cas.cos(x_error[2]))
-            state_cost = cas.mtimes(cas.mtimes(x_error.T, self.cas_state_cost_matrix), x_error)
-            u_error = self.u_ref[i - 1, :] - self.u_horizon[i - 1, :]
-            input_cost = cas.mtimes(cas.mtimes(u_error, self.cas_input_cost_matrix), u_error.T)
-            self.prediction_cost = self.prediction_cost + state_cost + input_cost
-
-        self.x_horizon = cas.hcat(self.x_horizon_list)
-        self.horizon_pred = cas.Function('horizon_pred', [self.x_0, self.u_horizon_flat], [self.x_horizon])
-        # self.pred_cost = cas.Function('pred_cost', [self.u_horizon_flat], [self.prediction_cost])
-
-        
-        self.nlp_params = cas.vertcat(self.x_0, self.x_ref_flat,self.cas_input_cost_param,
-        self.cas_state_cost_translational,self.cas_state_cost_rotationnal)
         self.lower_bound_input = np.full(2 * self.horizon_length, -self.max_wheel_vel)
         self.upper_bound_input = np.full(2 * self.horizon_length, self.max_wheel_vel)
-        self.optim_problem = {"f": self.prediction_cost, "x": self.u_horizon_flat, "p": self.nlp_params}
-        self.nlpsol_opts = {'verbose_init': False, 'print_in': False, 'print_out': False, 'print_time': False,
-                            'verbose': False, 'ipopt': {'print_level': 0}}
-        self.optim_problem_solver = cas.nlpsol("optim_problem_solver", "ipopt", self.optim_problem, self.nlpsol_opts)
-        # Casadi has been re_init. 
-        self.function_to_re_init = False
+        self.optim_commands_wheels = np.zeros((2, self.horizon_length))
+        self.optim_commands_body = np.zeros((2, self.horizon_length))
+        self.target_trajectory = np.zeros((3, self.horizon_length))
+        self.optim_trajectory = np.zeros((3, self.horizon_length))
+  
+        x = cas.SX.sym('x', 3)
+        u = cas.SX.sym('u', 2)
+
+        J = self.motion_model.jacobian_3x2
+        R = cas.vertcat(
+            cas.horzcat(cas.cos(x[2]), -cas.sin(x[2]), 0),
+            cas.horzcat(cas.sin(x[2]), cas.cos(x[2]), 0),
+            cas.horzcat(0, 0, 1)
+        )
+
+        x_0 = cas.SX.sym('x_0', 3, 1)
+        x_k = x + cas.mtimes(R, cas.mtimes(J, u)) / self.rate
+        single_step_pred = cas.Function('single_step_pred', [x, u], [x_k])
+
+        u_horizon_flat = cas.SX.sym('u_horizon_flat', 2 * self.horizon_length)
+        u_horizon = cas.reshape(u_horizon_flat, 2, self.horizon_length).T
+
+        x_ref_flat = cas.SX.sym('x_ref', 3 * self.horizon_length)
+        x_ref = cas.reshape(x_ref_flat, self.horizon_length, 3).T
+        u_ref = cas.DM(self.optim_commands_wheels.T)
+
+        velocity_delta_cost = cas.SX.sym('velocity_delta_cost', 1)
+        linear_state_cost = cas.SX.sym('linear_state_cost', 1)
+        angular_state_cost = cas.SX.sym('angular_state_cost', 1)
+
+        velocity_cost_matrix = cas.diag(cas.repmat(velocity_delta_cost, 2, 1))
+        state_cost_matrix = cas.diag(cas.vertcat(
+            linear_state_cost,
+            linear_state_cost,
+            angular_state_cost
+        ))
+        
+        x_horizon_list = [x_0]
+        prediction_cost = cas.SX(0)
+
+        for i in range(1, self.horizon_length):
+            x_horizon_list.append(single_step_pred(x_horizon_list[i - 1], u_horizon[i - 1, :]))
+            x_error = x_ref[:, i] - x_horizon_list[i]
+            x_error[2] = cas.atan2(cas.sin(x_error[2]), cas.cos(x_error[2]))
+            state_cost = cas.mtimes(cas.mtimes(x_error.T, state_cost_matrix), x_error)
+            u_error = u_ref[i - 1, :] - u_horizon[i - 1, :]
+            input_cost = cas.mtimes(cas.mtimes(u_error, velocity_cost_matrix), u_error.T)
+            prediction_cost = prediction_cost + state_cost + input_cost
+
+        x_horizon = cas.hcat(x_horizon_list)
+        self.horizon_pred = cas.Function('horizon_pred', [x_0, u_horizon_flat], [x_horizon])
+
+        nlp_params = cas.vertcat(
+            x_0, 
+            x_ref_flat, 
+            velocity_delta_cost, 
+            linear_state_cost, 
+            angular_state_cost
+        )
+        optim_problem = {
+            "f": prediction_cost, 
+            "x": u_horizon_flat, 
+            "p": nlp_params
+        }
+        nlpsol_opts = {
+            'verbose_init': False, 
+            'print_in': False, 
+            'print_out': False, 
+            'print_time': False,
+            'verbose': False, 
+            'ipopt': {'print_level': 0}
+        }
+        self.optim_problem_solver = cas.nlpsol("optim_problem_solver", "ipopt", optim_problem, nlpsol_opts)
 
 
     def compute_distance_to_goal(self, state, orthogonal_projection_id):
@@ -145,14 +126,13 @@ class IdealDiffDriveMPC(Controller):
     def compute_desired_trajectory(self, state):
         
         # Find closest point on path
-        closest_pose, self.next_path_idx = self.path.compute_orthogonal_projection(
+        self.closest_pose, self.next_path_idx = self.path.compute_orthogonal_projection(
             state, self.next_path_idx, self.id_window_size, self.maximum_linear_velocity, self.maximum_angular_velocity
         )
 
-        self.closest_pose = closest_pose
         # Find the points on the path that are accessible within the horizon
         horizon_duration = self.horizon_length / self.rate
-        horizon_poses, cumul_duration = self.path.compute_horizon(closest_pose, self.next_path_idx, horizon_duration, self.maximum_linear_velocity, self.maximum_angular_velocity)
+        horizon_poses, cumul_duration = self.path.compute_horizon(self.closest_pose, self.next_path_idx, horizon_duration, self.maximum_linear_velocity, self.maximum_angular_velocity)
         horizon_duration = min(horizon_duration, cumul_duration[-1])
         
         # Interpolate the poses to get the desired trajectory
@@ -169,56 +149,47 @@ class IdealDiffDriveMPC(Controller):
 
         self.planar_state = np.array([state[0], state[1], state[5]])
         self.compute_desired_trajectory(self.planar_state)
-        nlp_params = np.concatenate((self.planar_state, self.target_trajectory.flatten('C'),
-            np.array([self.input_cost_wheel]), np.array([self.state_cost_translational]),
-            np.array([self.state_cost_rotational])
+
+        nlp_params = np.concatenate((
+            self.planar_state, 
+            self.target_trajectory.flatten(),
+            np.array([self.velocity_delta_cost]), 
+            np.array([self.linear_state_cost]),
+            np.array([self.angular_state_cost])
         )) 
+
         self.optim_control_solution = self.optim_problem_solver(
-            x0=self.previous_input_array.flatten(),
+            x0=self.optim_commands_wheels.flatten(),
             p=nlp_params,
             lbx= self.lower_bound_input,
             ubx= self.upper_bound_input
         )['x']
 
-        self.optimal_left = self.optim_control_solution[0]
-        self.optimal_right = self.optim_control_solution[self.horizon_length]
-        wheel_input_array = np.array([self.optimal_left, self.optimal_right]).reshape(2,1)
-        body_input_array = self.motion_model.compute_body_vel(wheel_input_array).astype('float64')
+        self.optim_commands_wheels = np.array(self.optim_control_solution).reshape(2, self.horizon_length)
+        self.optim_commands_body = self.motion_model.compute_body_vel(self.optim_commands_wheels).astype('float64')
+        self.optim_trajectory = np.array(self.horizon_pred(self.planar_state, self.optim_control_solution))
 
-        optim_trajectory = self.horizon_pred(self.planar_state, self.optim_control_solution)
-        self.optim_solution_array = np.array(self.optim_control_solution)
-        self.optim_trajectory_array[0:3, :] = optim_trajectory[0:3, :]
-        self.previous_input_array[0, :] = self.optim_solution_array[:self.horizon_length].reshape(self.horizon_length)
-        self.previous_input_array[1, :] = self.optim_solution_array[self.horizon_length:].reshape(self.horizon_length)
         self.compute_distance_to_goal(state, self.next_path_idx)
         self.next_command_id = 1
-        return body_input_array.reshape(2)
+
+        return self.optim_commands_body[:, 0]
     
 
     def get_next_command(self):
 
         if self.next_command_id < self.horizon_length:
-            self.optimal_left = self.optim_control_solution[self.next_command_id]
-            self.optimal_right = self.optim_control_solution[self.next_command_id + self.horizon_length]
-            wheel_input_array = np.array([self.optimal_left, self.optimal_right]).reshape(2,1)
-            body_input_array = self.motion_model.compute_body_vel(wheel_input_array).astype('float64')
+            next_command = self.optim_commands_body[:, self.next_command_id]
             self.next_command_id += 1
         else:
-            body_input_array = np.array([0.0, 0.0]) # Stop if we never receive odom
-        return body_input_array.reshape(2), self.next_command_id-1
+            next_command = np.array([0.0, 0.0]) # Stop if we never receive odom
+
+        return next_command.reshape(2), self.next_command_id - 1
 
 
     def goal_reached(self):
 
-        if (self.linear_distance_to_goal < self.linear_goal_tolerance) and (np.abs(self.angular_distance_to_goal) < self.angular_goal_tolerance): 
-            
-            return True
-        
-        elif self.linear_distance_to_goal < self.linear_goal_tolerance:
-            self.debug_indicator[0] = True
-        elif np.abs(self.angular_distance_to_goal) < self.angular_goal_tolerance:
-            self.debug_indicator[1] = True
-        
+        return (self.linear_distance_to_goal < self.linear_goal_tolerance and 
+                self.angular_distance_to_goal < self.angular_goal_tolerance)       
 
 
 if __name__ == "__main__":
@@ -229,14 +200,13 @@ if __name__ == "__main__":
         'maximum_linear_velocity': 2.0,
         'minimum_linear_velocity': 0.1,
         'maximum_angular_velocity': 1.0,
-        'goal_tolerance': 0.5,
-        'path_look_ahead_distance': 2.0,
-        'id_window_size': 50,
+        'linear_goal_tolerance': 0.5,
+        'angular_goal_tolerance': 0.5,
+        'id_window_size': 10,
         'horizon_length': 40,
-        'state_cost_translational': 1.0,
-        'state_cost_rotational': 0.2,
-        'input_cost_wheel': 0.001,
-        'angular_velocity_gain': 1.0,
+        'linear_state_cost': 1.0,
+        'angular_state_cost': 0.2,
+        'velocity_delta_cost': 0.001,
         'wheel_radius': 0.3,
         'baseline': 1.2,
         'rate': 20.0,
@@ -295,25 +265,23 @@ if __name__ == "__main__":
 
     dummy_path = reverse_path(dummy_path)
 
-    robot_pose = np.array([0.5, -0.1, 0.0, 0.0, 0.0, -1.0])
+    robot_pose = np.array([0.2, -0.3, 0.0, 0.0, 0.0, -1.5])
 
     controller = IdealDiffDriveMPC(parameter_map)
     controller.update_path(Path(dummy_path))
     controller.compute_command_vector(robot_pose)
-
-    # print('Target trajectory:', controller.target_trajectory)
 
     def on_key_press(event):
 
         global controller, robot_pose
         
         robot_pose = [
-            controller.optim_trajectory_array[0, 1],
-            controller.optim_trajectory_array[1, 1],
+            controller.optim_trajectory[0, 1],
+            controller.optim_trajectory[1, 1],
             0.0,
             0.0,
             0.0,
-            wrap2pi(controller.optim_trajectory_array[2, 1])
+            wrap2pi(controller.optim_trajectory[2, 1])
         ]
         controller.compute_command_vector(robot_pose)
 
@@ -327,14 +295,14 @@ if __name__ == "__main__":
         plt.scatter(controller.target_trajectory[0, 0], controller.target_trajectory[1, 0], c='g', label='Orthogonal Projection')
         plt.plot(controller.target_trajectory[0, 1:], controller.target_trajectory[1, 1:], 'bo', label='Horizon Trajectory')
         plt.quiver(controller.target_trajectory[0, 1:], controller.target_trajectory[1, 1:], np.cos(controller.target_trajectory[2, 1:]), np.sin(controller.target_trajectory[2, 1:]), color='b', label='Horizon Trajectory')
-        plt.plot(controller.optim_trajectory_array[0, 1:], controller.optim_trajectory_array[1, 1:], 'yo', label='Optimal Trajectory')
-        plt.quiver(controller.optim_trajectory_array[0, 1:], controller.optim_trajectory_array[1, 1:], np.cos(controller.optim_trajectory_array[2, 1:]), np.sin(controller.optim_trajectory_array[2, 1:]), color='y', label='Optimal Trajectory')
+        plt.plot(controller.optim_trajectory[0, 1:], controller.optim_trajectory[1, 1:], 'yo', label='Optimal Trajectory')
+        plt.quiver(controller.optim_trajectory[0, 1:], controller.optim_trajectory[1, 1:], np.cos(controller.optim_trajectory[2, 1:]), np.sin(controller.optim_trajectory[2, 1:]), color='y', label='Optimal Trajectory')
         plt.scatter(robot_pose[0], robot_pose[1], c='k', label='Robot Pose')
         plt.quiver(robot_pose[0], robot_pose[1], np.cos(robot_pose[5]), np.sin(robot_pose[5]), color='k')
         plt.axis('equal')
         plt.quiver(controller.closest_pose[0],controller.closest_pose[1],np.cos(controller.closest_pose[2]),np.sin(controller.closest_pose[2]),color="cyan",label="closest pose",zorder=10)
         plt.legend()
-        plt.ylim(-3.5,-2.5)
+        plt.ylim(-3.5,0.0)
         plt.xlim(-1,0.5)
 
 
